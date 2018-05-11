64e6427049767ce159831b0559ce99787178558a
gbarcelo <gbarcelo@users.noreply.github.com>
2018-05-11 00:18:32 -1000

    implemented TickType

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,4 +1,5 @@
 #include "GameEngine.hpp"
+#include <iostream>
 
 GameEngine::GameEngine(GameMode game_, GameState gameState_) :
     game(game_), gameState(gameState_) {
@@ -7,10 +8,18 @@ GameEngine::GameEngine(GameMode game_, GameState gameState_) :
 
 void GameEngine::setUp(){
   //
+
+  // SetUp Complete, begin main game loop
+  if (game.TickType == Rounds){
+    GameEngine::mainLoop_Rounds();
+  } else {
+    std::cout << "Error: Game TickType: Not yet implemented" << std::endl;
+  }
 }
 
-void GameEngine::mainLoop(){
+void GameEngine::mainLoop_Rounds(){
   //
+
 }
 
 void GameEngine::sessionSummary(){
